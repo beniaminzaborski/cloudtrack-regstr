d3f3612850789d96c4de871375745d5bdfa82add
Beniamin Zaborski <beniamin.zaborski@gmail.com>
2025-01-31 16:49:48 +0100

    Align Azure Function App

--- src/CloudTrack.Registration.NumberAssignatorAzFunction/Program.cs
+++ src/CloudTrack.Registration.NumberAssignatorAzFunction/Program.cs
@@ -1,7 +1,27 @@
+using CloudTrack.Registration.Application.CompetitorRegistration;
+using CloudTrack.Registration.NumberAssignatorAzFunction;
+using CloudTrack.Registration.NumberAssignatorAzFunction.Consumers;
+using MassTransit;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices(services =>
+    {
+        services
+            .AddApplicationInsightsTelemetryWorkerService()
+            .ConfigureFunctionsApplicationInsights()
+            .AddScoped<ICompetitorService, CompetitorService>()
+            .AddScoped<NumberAssignatorFunction>()
+            .AddMassTransitForAzureFunctions(cfg =>
+            {
+                cfg.AddConsumersFromNamespaceContaining<ConsumerNamespace>();
+                cfg.AddRequestClient<RegisterCompetitor>(new Uri($"queue:{NumberAssignatorFunction.QueueName}"));
+            }, "ServiceBusConnectionString");
+    })
     .Build();
 
 host.Run();
