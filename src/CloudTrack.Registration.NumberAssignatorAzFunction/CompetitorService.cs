53af7cffc36cc17d660d5866f2fe65e98adcb7ab
Beniamin Zaborski <beniamin.zaborski@gmail.com>
2025-03-06 19:34:22 +0100

    Refactoring Azure Function

--- src/CloudTrack.Registration.NumberAssignatorAzFunction/CompetitorService.cs
+++ src/CloudTrack.Registration.NumberAssignatorAzFunction/CompetitorService.cs
@@ -9,24 +9,26 @@ namespace CloudTrack.Registration.NumberAssignatorAzFunction;
 
 internal class CompetitorService(ILogger<CompetitorService> logger) : ICompetitorService
 {
-    readonly ILogger<CompetitorService> _logger = logger;
+    private readonly ILogger<CompetitorService> _logger = logger;
 
     public async Task<(Guid id, string number)> RegisterCompetitorAndReturnNumber(RegisterCompetitor competitor)
     {
-        using var connection = CreateConnection();
+        await using var connection = CreateConnection();
         await connection.OpenAsync();
-        var transaction = await connection.BeginTransactionAsync();
+        await using var transaction = await connection.BeginTransactionAsync();
+
         try
         {
             var id = Guid.NewGuid();
 
-            var maxNumerOfCompetitors = await GetMaxNumberOfCompetitorsAsync(competitor.CompetitionId);
-            var numerOfRegistrations = await GetNumberOfRegisteredCompetitorsAsync(competitor.CompetitionId);
-            if (numerOfRegistrations >= maxNumerOfCompetitors) throw new Exception("Registrations have reached the limit");
-            
-            var number = await GetNextNumberAsync(competitor.CompetitionId);
-            
-            await InsertCompetitorAsync(competitor, id, number);
+            if (await HasReachedMaxRegistrationsAsync(connection, transaction, competitor.CompetitionId))
+            {
+                throw new Exception("Registrations have reached the limit");
+            }
+
+            var number = await GetNextNumberAsync(connection, transaction, competitor.CompetitionId);
+            await InsertCompetitorAsync(connection, transaction, competitor, id, number);
+
             await transaction.CommitAsync();
             return (id, number.ToString());
         }
@@ -34,56 +36,61 @@ internal class CompetitorService(ILogger<CompetitorService> logger) : ICompetito
         {
             _logger.LogError($"Something went wrong for request id: {competitor.RequestId}. Details: {ex.Message}");
             await transaction.RollbackAsync();
+            return (Guid.Empty, string.Empty);
         }
+    }
 
-        return (Guid.Empty, string.Empty);
+    private static async Task<bool> HasReachedMaxRegistrationsAsync(NpgsqlConnection connection, NpgsqlTransaction transaction, Guid competitionId)
+    {
+        var maxCompetitors = await GetMaxNumberOfCompetitorsAsync(connection, transaction, competitionId);
+        var registeredCompetitors = await GetNumberOfRegisteredCompetitorsAsync(connection, transaction, competitionId);
+        return registeredCompetitors >= maxCompetitors;
+    }
 
-        async Task<int> GetMaxNumberOfCompetitorsAsync(Guid competitionId)
+    private static async Task<int> GetMaxNumberOfCompetitorsAsync(NpgsqlConnection connection, NpgsqlTransaction transaction, Guid competitionId)
+    {
+        const string sql = "SELECT \"maxCompetitors\" FROM competitions WHERE \"id\" = @competitionId;";
+        return await connection.ExecuteScalarAsync<int>(sql, new { competitionId }, transaction);
+    }
+
+    private static async Task<int> GetNumberOfRegisteredCompetitorsAsync(NpgsqlConnection connection, NpgsqlTransaction transaction, Guid competitionId)
+    {
+        const string sql = "SELECT COUNT(id) FROM competitors WHERE \"competitionId\" = @competitionId;";
+        return await connection.ExecuteScalarAsync<int>(sql, new { competitionId }, transaction);
+    }
+
+    private static async Task<long> GetNextNumberAsync(NpgsqlConnection connection, NpgsqlTransaction transaction, Guid competitionId)
+    {
+        const string sql = @"
+            CALL generate_next_number(@competitionId);
+            SELECT ""currentNumber"" FROM numerators WHERE ""competitionId"" = @competitionId;";
+        return await connection.ExecuteScalarAsync<long>(sql, new { competitionId }, transaction);
+    }
+
+    private static async Task InsertCompetitorAsync(NpgsqlConnection connection, NpgsqlTransaction transaction, RegisterCompetitor registerCompetitor, Guid id, long number)
+    {
+        const string sql = @"
+            INSERT INTO competitors (""id"", ""requestId"", ""competitionId"", ""number"", ""firstName"", ""lastName"", ""birthDate"", ""city"", ""phoneNumber"", ""contactPersonNumber"")
+            VALUES (@id, @requestId, @competitionId, @number, @firstName, @lastName, @birthDate, @city, @phoneNumber, @contactPersonNumber);";
+        
+        await connection.ExecuteAsync(sql, new
         {
-            var sql = @"
-                select ""maxCompetitors"" from competitions c where c.""id"" = @competitionId;";
-            return await connection.ExecuteScalarAsync<int>(sql, new { competitionId }, transaction);
-        }
+            id,
+            requestId = registerCompetitor.RequestId,
+            competitionId = registerCompetitor.CompetitionId,
+            number,
+            firstName = registerCompetitor.FirstName,
+            lastName = registerCompetitor.LastName,
+            birthDate = registerCompetitor.BirthDate,
+            city = registerCompetitor.City,
+            phoneNumber = registerCompetitor.PhoneNumber,
+            contactPersonNumber = registerCompetitor.ContactPersonNumber
+        }, transaction);
+    }
 
-        async Task<int> GetNumberOfRegisteredCompetitorsAsync(Guid competitionId)
-        {
-            var sql = @"
-                select count(id) from competitors c where c.""competitionId"" = @competitionId;";
-            return await connection.ExecuteScalarAsync<int>(sql, new { competitionId }, transaction);
-        }
-
-        async Task<long> GetNextNumberAsync(Guid competitionId)
-        {
-            var sql = @"
-                call generate_next_number(@competitionId);
-                select ""currentNumber"" from numerators where ""competitionId"" = @competitionId;";
-            return await connection.ExecuteScalarAsync<long>(sql, new { competitionId }, transaction);
-        }
-
-        async Task InsertCompetitorAsync(RegisterCompetitor registerCompetitor, Guid id, long number)
-        {
-            var sql = @"
-                INSERT INTO competitors (""id"", ""requestId"", ""competitionId"", ""number"", ""firstName"", ""lastName"", ""birthDate"", ""city"", ""phoneNumber"", ""contactPersonNumber"") 
-                VALUES (@id, @requestId, @competitionId, @number, @firstName, @lastName, @birthDate, @city, @phoneNumber, @contactPersonNumber)";
-            await connection.ExecuteAsync(sql, new
-            {
-                id,
-                requestId = registerCompetitor.RequestId,
-                competitionId = registerCompetitor.CompetitionId,
-                number,
-                firstName = registerCompetitor.FirstName,
-                lastName = registerCompetitor.LastName,
-                birthDate = registerCompetitor.BirthDate,
-                city = registerCompetitor.City,
-                phoneNumber = registerCompetitor.PhoneNumber,
-                contactPersonNumber = registerCompetitor.ContactPersonNumber
-            }, transaction);
-        }
-
-        NpgsqlConnection CreateConnection()
-        {
-            var connectionString = Environment.GetEnvironmentVariable("PostgresConnectionString", EnvironmentVariableTarget.Process);
-            return new NpgsqlConnection(connectionString);
-        }
+    private static NpgsqlConnection CreateConnection()
+    {
+        var connectionString = Environment.GetEnvironmentVariable("PostgresConnectionString", EnvironmentVariableTarget.Process);
+        return new NpgsqlConnection(connectionString);
     }
 }
